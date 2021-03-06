442dd12767fb24aec9d0712745fda8e096341f6f
Amritesh Khare <amritesh.16609@knit.ac.in>
2021-03-07 01:06:26 +0530

    Updates

---
+++ precache-manifest.a5ea61ffd450515bea336f2f424b6d64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1965b756ca6d0656b09eb943ed96e466",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e2920b07d08372964ff6",
+    "url": "/react-portfolio/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "09c24e85c6ae81d15ee9",
+    "url": "/react-portfolio/static/css/main.fb024bf3.chunk.css"
+  },
+  {
+    "revision": "e2920b07d08372964ff6",
+    "url": "/react-portfolio/static/js/2.c360366e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-portfolio/static/js/2.c360366e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c24e85c6ae81d15ee9",
+    "url": "/react-portfolio/static/js/main.0bf862b9.chunk.js"
+  },
+  {
+    "revision": "fc28123deae08e53ec3a",
+    "url": "/react-portfolio/static/js/runtime-main.ae70f43c.js"
+  },
+  {
+    "revision": "bb038eeaacaf003b65bacad9ac90a511",
+    "url": "/react-portfolio/static/media/1ml.bb038eea.png"
+  }
+]);
